59175e67c3dae065a87f24ce59fc754159257229
kyorkston <kyorkston@gmail.com>
2020-10-12 17:09:01 +0100

    remove unused code

--- apple/RNCWebView.m
+++ apple/RNCWebView.m
@@ -224,10 +224,6 @@ static NSDictionary* customCertificatesForHost;
   if(self.useSharedProcessPool) {
     wkWebViewConfig.processPool = [[RNCWKProcessPoolManager sharedManager] sharedProcessPool];
   }
-  if (@available(iOS 13.0, *), @available is UIDocumentMenuViewController && UIDevice.current.userInterfaceIdiom == .phone) {
-      UIViewController.popO
-    wkWebViewConfig.popO
-  }
   wkWebViewConfig.userContentController = [WKUserContentController new];
 
 #if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 130000 /* iOS 13 */
